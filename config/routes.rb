26918da950181fac2be2350a75815b6fa18c32ea
majestictree <unlimited.blade.works22@gmail.com>
2023-03-02 09:32:04 +0900

    create todo_controller actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,15 @@
 Rails.application.routes.draw do
+
+  root to: redirect('/todos')
+
+  get 'todos', to: 'site#index'
+  get 'todos/new', to: 'site#index'
+  get 'todos/:id/edit', to: 'site#index'
+
   namespace :api do
     namespace :v1 do
-      get 'todos/index'
-      get 'todos/show'
-      get 'todos/create'
-      get 'todos/update'
-      get 'todos/destroy'
-      get 'todos/destroy_all'
+      delete '/todos/destroy_all', to: 'todos#destroy_all'
+      resources :todos, only: %i[index show create update destroy]
     end
   end
-  get 'sites/index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
